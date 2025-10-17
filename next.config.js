7cbc21ab46f4755ea6d297b3528fe9d75bb6c5ab
christophe-korv <christophe.danwe@korv-consulting.fr>
2025-10-17 17:39:28 +0100

    Updating Next Config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    output: 'export', // nécessaire pour GitHub Pages
+    // images: {
+    //     unoptimized: true, // désactive l’optimisation d’images côté serveur
+    // },
+    basePath: '/marketflow', // le nom de ton dépôt GitHub
+    assetPrefix: '/marketflow/', // préfixe pour les ressources
+};
 
 module.exports = nextConfig;
